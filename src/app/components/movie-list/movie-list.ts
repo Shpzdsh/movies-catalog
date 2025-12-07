a8ea389b8e8df692d5456dddc7bc1d799cc624b5
shpzdsh <valya-sidd1999@yandex.ru>
2025-12-08 02:39:15 +0400

    add quick view

--- src/app/components/movie-list/movie-list.ts
+++ src/app/components/movie-list/movie-list.ts
@@ -3,12 +3,12 @@ import { MovieService } from '../../services/movie.service';
 import { Router } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { BehaviorSubject, catchError, debounceTime, distinctUntilChanged, map, Observable, Subject, switchMap, tap, throwError } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, DecimalPipe ],
   templateUrl: './movie-list.html',
   styleUrl: './movie-list.scss',
 })
@@ -32,8 +32,47 @@ export class MovieList {
     })
   );
 
-  protected handleError(error: Error) {
-    console.error(error);
-    return error;
+  protected hoveredMovieId: string | null = null;
+  protected hoveredTimeout: any = null;
+  protected showQuickView = false;
+
+  protected quickViewPosition = { x: 0, y: 0 };
+
+  protected onMovieHover(event: MouseEvent, movie: any): void {
+    if (this.hoveredTimeout) {
+      clearTimeout(this.hoveredTimeout);
+    }
+
+    this.hoveredMovieId = movie.id;
+    this.quickViewPosition = {
+      x: event.clientX + 20,
+      y: event.clientY + 20
+    };
+
+    this.hoveredTimeout = setTimeout(() => {
+      this.showQuickView = true;
+    }, 300);
   }
+
+  protected onMovieLeave(): void {
+    if (this.hoveredTimeout) {
+      clearTimeout(this.hoveredTimeout);
+    }
+
+    this.hoveredTimeout = setTimeout(() => {
+      this.showQuickView = false;
+      this.hoveredMovieId = null;
+    }, 200);
+  }
+
+  protected onQuickViewEnter(): void {
+    if (this.hoveredTimeout) {
+      clearTimeout(this.hoveredTimeout);
+    }
+  }
+
+  protected onQuickViewLeaved(): void {
+    this.onMovieLeave();
+  }
+
 }
